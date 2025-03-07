442b3a4e26a8d53404fbffd3017ef13b147aa076
Renzhijie110 <97765487+Renzhijie110@users.noreply.github.com>
2025-03-07 13:00:07 -0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,9 +12,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <Link href="/" className="text-xl font-bold text-gray-800">
                 物品追踪系统
-              </Link>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
               <Link
